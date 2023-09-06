2298db69efaeb6a78fe13d90c949a388460a872b
ananth-labs <144272483+ananth-labs@users.noreply.github.com>
2023-09-06 19:26:58 +0100

    Update 04_find_replace.py

--- python_labs/04_find_replace.py
+++ python_labs/04_find_replace.py
@@ -1,29 +1,24 @@
+# import required packages
 import re
 
 # Creating a function to replace the text
 def replacetext(source_file, search_text,replace_text):
-
 	# Opening the file in read and write mode
 	with open(source_file,'r+') as f:
-
 		# Reading the file data and store it in a file variable
 		file = f.read()
-		
 		# Replacing the pattern with the string in the file data
 		file = re.sub(search_text, replace_text, file)
-
 		# Setting the position to the top of the page to insert data
 		f.seek(0)
-		
 		# Writing replaced data in the file
 		f.write(file)
-
 		# Truncating the file size
 		f.truncate()
-
 	# Return "Text replaced" string
 	return "String successfully replaced"
 
+# Script usagee from the above function
 # File to search and replace string
 file_path = "C:\Tark_Files\WorkFiles\python demos\my_api_packs\source\sample1.txt"
 
