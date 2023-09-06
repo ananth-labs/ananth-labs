64072e525bd8f964aec1a8841119400f32e0d264
ananth-labs <144272483+ananth-labs@users.noreply.github.com>
2023-09-06 19:36:32 +0100

    Update 01_unzip_api_pack.py

--- python_labs/01_unzip_api_pack.py
+++ python_labs/01_unzip_api_pack.py
@@ -1,12 +1,13 @@
+# Import required package
 import zipfile
 def unzip_api_pack(apiPackage, destinationfolder):
     with zipfile.ZipFile(apiPackage, 'r') as zip_ref:
         zip_ref.extractall(destinationfolder)
     print ("FIle extracted successfully")
 
-#Usage of above function
+# Script usage of above function
 print ("Preparing to extract files")
 print ("Any files starting with a will through error...")
-apizipfile = "C:\Tark_Files\WorkFiles\python demos\my_api_packs\source\my_apigee-samples-main.zip"
-outputdir = "C:\Tark_Files\WorkFiles\python demos\my_api_packs\output\samples-main"
+apizipfile = "C:\WorkFiles\python demos\my_api_packs\source\my_apigee-samples-main.zip"
+outputdir = "C:\WorkFiles\python demos\my_api_packs\output\samples-main"
 unzip_api_pack(apizipfile, outputdir)
