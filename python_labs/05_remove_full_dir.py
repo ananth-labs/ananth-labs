025842925e2da57a0a198b62d5e0f6db7fb8198c
ananth-labs <144272483+ananth-labs@users.noreply.github.com>
2023-09-06 19:33:44 +0100

    Update 05_remove_full_dir.py

--- python_labs/05_remove_full_dir.py
+++ python_labs/05_remove_full_dir.py
@@ -1,12 +1,17 @@
+# Import required packages
 import shutil
 import os
 
-# location
+def delete_full_dir_file(dir_path):
+    # location
+    if os.path.exists(dir_path):
+        # removing directory
+        shutil.rmtree(dir_path, ignore_errors=False)
+        # making ignore_errors = True will not raise a FileNotFoundError
+        print("The folder has been deleted successfully!")
+    else:
+        print("Can not delete the folder as it doesn't exists")
+
+# Usage of the above function
 folderPath = "C:/Tark_Files/WorkFiles/python demos/my_api_packs/output/samples-main"
-if os.path.exists(folderPath):
-    # removing directory
-    shutil.rmtree(folderPath, ignore_errors=False)
-    # making ignore_errors = True will not raise a FileNotFoundError
-    print("The folder has been deleted successfully!")
-else:
-    print("Can not delete the folder as it doesn't exists")
+delete_full_dir_file(folderPath)
